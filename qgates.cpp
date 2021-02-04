f90b7923cadf3ec28c845411d0adbed417159519
atharvak71 <69001125+atharvak71@users.noreply.github.com>
2021-02-04 14:27:24 +0530

    Create qgates.cpp

---
+++ qgates.cpp
@@ -0,0 +1,10 @@
+#include "qgates.h"
+/*
+class qrotate:public qgate
+{
+public:
+	qrotate(const double& theta);
+	virtual void apply(std::complex<double>* out,const std::complex<double*> in) const;
+protected:
+	qrotate(const double& theta,const std::string&);
+};*/
